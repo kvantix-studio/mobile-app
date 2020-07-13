559be0199f9c21a123d233a27c7a8b8f20680b02
Evgeny <salabaikin@mail.ru>
2020-07-13 12:49:51 +0300

    fix

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,43 @@ $('.main-slider').slick({
     arrows: false,
     autoplay: true
 });
+
+$('.card-product-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dots: true,
+    arrows: false
+});
+
+$('.similar-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    dots: false,
+    arrows: false,
+    autoplay: true,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 3
+            }
+        },
+        {
+          breakpoint: 570,
+          settings: {
+            slidesToShow: 2
+          }
+        }
+      ]
+});
+
+window.onscroll = function() {
+  var scrolled = window.pageYOffset || document.documentElement.scrollTop;
+  if (scrolled !== 0) {
+    document.querySelector('.header--card').classList.remove('header--transparent');
+  } else {
+    document.querySelector('.header--card').classList.add('header--transparent');
+  }
+};
